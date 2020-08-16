d780e103991d6436240334afaeae7a0311580709
Danilo Ribeiro <daniloribeiro@DMAX101MacMini.local>
2020-08-15 21:47:54 -0300

    Voice implementation

--- src/component/voiceButton/index.tsx
+++ src/component/voiceButton/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { View, Image } from 'react-native';
-import Voice from "@react-native-community/voice";
+import * as Speech from 'expo-speech'; // Talk to user lib
+import * as Permissions from 'expo-permissions';
 
 import { BorderlessButton } from 'react-native-gesture-handler';
 
@@ -11,8 +12,29 @@ import voiceButtonIcon from '../../../assets/icon/voiceButton.png';
 import styles from './styles';
 
 function VoiceButton() {
+    async function alertIfMicrophonePermissionDisabledAsync() {
+        const { status } = await Permissions.getAsync(Permissions.AUDIO_RECORDING);
+        if (status !== 'granted') {
+          alert('Uso do microfone não permitido');
+        }
+    }
+
     function handleVoiceCommand() {
         console.log("Pressionou o Botão!");
+        alertIfMicrophonePermissionDisabledAsync()
+
+        try {
+            const options = {
+                'language': 'pt-BR',
+                'pitch': 1,
+                'rate': 1,
+            }
+            Speech.speak('Olá, Meu nome é Nateli', options);
+            
+        } catch (error) {
+            console.log('Algo deu errado!');
+            console.log(error);
+        }
 
          api.get('/message?v=20200811&q=ligar luz da garagem')
             .then((response) => {
