3294ed60f53110d7ddd6856b28761230b5c39964
dmax101 <dmax@DMAX101N.localdomain>
2020-10-01 15:44:01 -0300

    Clean

--- src/lib/mqtt/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import Message from './src/Message';
-import Client from './src/Client';
-
-export {
-  Message,
-  Client
-};
--- src/lib/mqtt/index.ts
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const Client = require('./src/Client.js');
-const Message = require('./src/Message.js');
-
-export default {
-    Client,
-    Message
-}
